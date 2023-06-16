3d8ae8bbd0927609a46f256ee8309ebb31c1f985
Arjun Singh <arjunsinghbazal@gmail.com>
2023-06-16 11:46:54 +0530

    submitting code-Fri Jun 16 2023 11:46:53 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,17 @@ import React, { useState } from "react";
 import './../styles/App.css';
 import Parent from "./Parent";
 const App = () => {
-<Parent/>
+const [Value,setValue]=useState("");
+const Data=(e)=>{
+  setValue(e.target.value)
+};
+  return (
+    <div id="parent">
+       <h1>Parent Component</h1>
+       <p>{Value}</p>
+       <Child value={Data}/>
+    </div>
+  )
 }
 
 export default App
--- src/components/Child.js
+++ src/components/Child.js
@@ -1,15 +1,10 @@
-import React from 'react';
+import React from "react";
 
-const Child = (props) => {
-    const {inputValue, setInputValue} = props;
-
-    return (
-        <div className="child">
-            <h2>Child Component</h2>
-            <input type="text" onChange={(e) => {
-                setInputValue(e.target.value);
-            }} value={inputValue} />
-        </div>
-    )
+const Child=({value})=>{
+return(
+   <div id="child"><h1>Child Component</h1>
+   <input type="text" onChange={value}/></div>
+)
 }
+
 export default Child;
