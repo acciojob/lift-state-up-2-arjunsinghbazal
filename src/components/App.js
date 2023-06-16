dbc4f0300f112660a6fe5c70fffbc5eb5f256250
Arjun Singh <arjunsinghbazal@gmail.com>
2023-06-16 12:08:48 +0530

    submitting code-Fri Jun 16 2023 12:08:47 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 
 import React, { useState } from "react";
 import './../styles/App.css';
-import Parent from "./Parent";
+import Child from "./Child";
 const App = () => {
 const [Value,setValue]=useState("");
 const Data=(e)=>{
